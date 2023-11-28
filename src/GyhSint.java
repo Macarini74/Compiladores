28a592200db101be5c109d98e0fe8011e7165a8b
Macarini74 <iagomcarini@alunos.utfpr.edu.br>
2023-11-28 09:18:52 -0300

    Correção Analisador Sintático

--- src/GyhSint.java
+++ src/GyhSint.java
@@ -95,8 +95,6 @@ public class GyhSint {
             System.exit(0);
         }
 
-
-
         if(this.tokens.get(index).equals("FimDelim")){
             System.out.println("Token: " + this.tokens.get(index) + "\n");
             index++;
@@ -133,10 +131,14 @@ public class GyhSint {
     //ExpressaoAritmetica → TermoAritmetico ExpressaoAritmetica'' ;
     public void expressaoAritmetica(){
         termoAritmetico();
-        expressaoAritmetica2();
+        if(this.tokens.get(index).equals("OpAritSoma") || this.tokens.get(index).equals("OpAritSub")){
+            expressaoAritmetica2();
+        }
+
     }
     //ExpressaoAritmetica'' → '+' TermoAritmetico ExpressaoAritmetica''|'-' TermoAritmetico ExpressaoAritmetica''| simboloVazio ;
     public void expressaoAritmetica2(){
+        System.out.println("Tokeneeeee: " + this.tokens.get(index));
         if(this.tokens.get(index).equals("OpAritSoma")){
             System.out.println("Token: " + this.tokens.get(index) + "\n");
             index++;
@@ -161,7 +163,9 @@ public class GyhSint {
     //TermoAritmetico → FatorAritmetico TermoAritmetico'';
     public void termoAritmetico(){
         fatorAritmetico();
-        termoAritmetico2();
+        if(this.tokens.get(index).equals("OpAritMul") || this.tokens.get(index).equals("OpAritDiv")){
+            termoAritmetico2();
+        }
     }
     //TermoAritmetico'' → '*' FatorAritmetico TermoAritmetico''|'/' FatorAritmetico TermoAritmetico'' | simboloVazio ;
     public void termoAritmetico2(){
@@ -171,6 +175,7 @@ public class GyhSint {
             fatorAritmetico();
             termoAritmetico2();
         }else {
+            System.out.println("Entrando: " + this.tokens.get(index));
             System.out.println("Erro Sintatico Linha: " + this.line.get(index) +  "Token: " + this.tokens.get(index) + "\n");
             System.exit(0);
         }
@@ -188,47 +193,47 @@ public class GyhSint {
 
     //FatorAritmetico → NUMINT| NUMREAL | VARIAVEL | '(' ExpressaoAritmetica ')' ;
     public void fatorAritmetico(){
-        if(this.tokens.get(index).equals("NumInt")){
-            System.out.println("Token: " + this.tokens.get(index) + "\n");
-            index++;
-        }else if(this.tokens.get(index).equals("NumReal")){
-            System.out.println("Token: " + this.tokens.get(index) + "\n");
-            index++;
-        }else if(this.tokens.get(index).equals("Var")){
-            System.out.println("Token: " + this.tokens.get(index) + "\n");
-            index++;
-        }else{
-            System.out.println("Erro Sintatico Linha: " + this.line.get(index) +  "Token: " + this.tokens.get(index) + "\n");
-            System.exit(0);
-        }
+        System.out.println("IDnsad" + this.tokens.get(index));
+        switch(this.tokens.get(index)){
+            case "NumInt":
+            case "NumReal":
+            case "Var":
+                System.out.println("Token: " + this.tokens.get(index) + "\n");
+                index++;
+                break;
+            case "AbrePar":
+                System.out.println("Token: " + this.tokens.get(index) + "\n");
+                index++;
+                expressaoAritmetica();
+                if(this.tokens.get(index).equals("FechaPar")){
+                    System.out.println("Token: " + this.tokens.get(index) + "\n");
+                    index++;
+                }else{
+                    System.out.println("Erro Sintatico Linha: " + this.line.get(index) + "Token: " + this.tokens.get(index) + "\n");
+                    System.exit(0);
+                }
+                break;
+            default:
+                System.out.println("Erro Sintatico Linha: " + this.line.get(index) + "Token: " + this.tokens.get(index) + "\n");
+                System.exit(0);
 
-        if(this.tokens.get(index).equals("AbrePar")){
-            System.out.println("Token: " + this.tokens.get(index) + "\n");
-            index++;
-            expressaoAritmetica();
-        } else if (this.tokens.get(index).equals("FechaPar")) {
-            System.out.println("Token: " + this.tokens.get(index) + "\n");
-            index++;
-        }else{
-            System.out.println("Erro Sintatico Linha: " + this.line.get(index) +  "Token: " + this.tokens.get(index) + "\n");
         }
     }
 
     //ExpressaoRelacional → TermoRelacional ExpressaoRelacional'';
     public void expressaoRelacional(){
+
         termoRelacional();
         expressaoRelacional2();
     }
     //ExpressaoRelacional'' → OperadorBooleano TermoRelacional ExpressaoRelacional'' | simboloVazio ;
     public void expressaoRelacional2(){
-        if(this.tokens.get(index).equals("OpBoolE") || this.tokens.get(index).equals("OpBoolOu")){
-            operadorBooleano();
+        if(operadorBooleano()){
             termoRelacional();
             expressaoRelacional2();
         }
 
     }
-
     //TermoRelacional → ExpressaoAritmetica OP_REL ExpressaoAritmetica | '(' ExpressaoRelacional ')';
     public void termoRelacional(){
         switch (this.tokens.get(index)){
@@ -237,63 +242,56 @@ public class GyhSint {
             case "Var":
                 System.out.println("Token: " + this.tokens.get(index) + "\n");
                 index++;
-                switch (this.tokens.get(index)){
-                    case "OpRelMenor":
-                    case "OpRelMenorIgual":
-                    case "OpRelMaior":
-                    case "OpRelMaiorIgual":
-                    case "OpRelIgual":
-                    case "OpRelDif":
+                switch (this.tokens.get(index)) {
+                    case "OpRelMenor", "OpRelMenorIgual", "OpRelMaior", "OpRelMaiorIgual", "OpRelIgual", "OpRelDif" -> {
                         System.out.println("Token: " + this.tokens.get(index) + "\n");
                         index++;
                         expressaoAritmetica();
-                        break;
-                    default:
-                        System.out.println("Erro Sintatico Linha: " + this.line.get(index) +  "Token: " + this.tokens.get(index) + "\n");
+                    }
+                    default -> {
+                        System.out.println("Erro Sintatico Linha: " + this.line.get(index) + "Token: " + this.tokens.get(index) + "\n");
                         System.exit(0);
+                    }
                 }
                 break;
             case "AbrePar":
                 System.out.println("Token: " + this.tokens.get(index) + "\n");
                 index++;
-                switch (this.tokens.get(index)){
-                    case "OpRelMenor":
-                    case "OpRelMenorIgual":
-                    case "OpRelMaior":
-                    case "OpRelMaiorIgual":
-                    case "OpRelIgual":
-                    case "OpRelDif":
+                switch (this.tokens.get(index)) {
+                    case "OpRelMenor", "OpRelMenorIgual", "OpRelMaior", "OpRelMaiorIgual", "OpRelIgual", "OpRelDif" -> {
                         System.out.println("Token: " + this.tokens.get(index) + "\n");
                         index++;
                         expressaoAritmetica();
-                        break;
-                    default:
+                    }
+                    default -> {
                         expressaoRelacional();
                         System.out.println("Token: " + this.tokens.get(index) + "\n");
                         index++;
-
-                        if(this.tokens.get(index).equals("FechaPar")){
+                        if (this.tokens.get(index).equals("FechaPar")) {
                             System.out.println("Token: " + this.tokens.get(index) + "\n");
                             index++;
-                        }else{
-                            System.out.println("Erro Sintatico Linha: " + this.line.get(index) +  "Token: " + this.tokens.get(index) + "\n");
+                        } else {
+                            System.out.println("Erro Sintatico Linha: " + this.line.get(index) + "Token: " + this.tokens.get(index) + "\n");
                             System.exit(0);
                         }
+                    }
                 }
         }
     }
 
     //OperadorBooleano → 'or' | 'and';
-    public void operadorBooleano(){
+    public Boolean operadorBooleano(){
         if(this.tokens.get(index).equals("OpBoolE")){
             System.out.println("Token: " + this.tokens.get(index) + "\n");
             index++;
+            return true;
         }else if(this.tokens.get(index).equals("OpBoolOu")){
             System.out.println("Token: " + this.tokens.get(index) + "\n");
             index++;
+            return true;
         }else{
             System.out.println("Erro Sintatico Linha: " + this.line.get(index) +  "Token: " + this.tokens.get(index) + "\n");
-            System.exit(0);
+            return false;
         }
     }
 
@@ -304,37 +302,30 @@ public class GyhSint {
     }
     //ListaComandos'' → ListaComandos| simboloVazio ;
     public void listaComandos2(){
-        if(this.tokens.get(index).equals("Var")) {
-            listaComandos();
-        }else if(this.tokens.get(index).equals("PCLer")) {
-            listaComandos();
-        }else if(this.tokens.get(index).equals("PCImprimir")){
-            listaComandos();
-        }else if(this.tokens.get(index).equals("PCEnqto")){
-            listaComandos();
-        }else if(this.tokens.get(index).equals("PCSe")){
-            listaComandos();
-        }else if(this.tokens.get(index).equals("PCIni")){
-            listaComandos();
+        switch (this.tokens.get(index)) {
+            case "Var" -> listaComandos();
+            case "PCLer" -> listaComandos();
+            case "PCImprimir" -> listaComandos();
+            case "PCEnqto" -> listaComandos();
+            case "PCSe" -> listaComandos();
+            case "PCIni" -> listaComandos();
         }
     }
 
 
     //Comando → ComandoAtribuicao | ComandoEntrada | ComandoSaida | ComandoCondicao | ComandoRepeticao | SubAlgoritmo;
     public void comando(){
-        if(this.tokens.get(index).equals("Var")) {
-            comandoAtribuicao();
-        }else if(this.tokens.get(index).equals("PCLer")) {
-            comandoEntrada();
-        }else if(this.tokens.get(index).equals("PCImprimir")) {
-            comandoSaida();
-        }else if(this.tokens.get(index).equals("PCEnqto")) {
-            comandoRepeticao();
-        }else if(this.tokens.get(index).equals("PCIni")){
-            subAlgoritmo();
-        }else{
-            System.out.println("Erro Sintatico Linha: " + this.line.get(index) +  "Token: " + this.tokens.get(index) + "\n");
-            System.exit(0);
+        switch (this.tokens.get(index)) {
+            case "Var" -> comandoAtribuicao();
+            case "PCLer" -> comandoEntrada();
+            case "PCImprimir" -> comandoSaida();
+            case "PCEnqto" -> comandoRepeticao();
+            case "PCIni" -> subAlgoritmo();
+            case "PCSe" ->  comandoCondicao();
+            default -> {
+                System.out.println("Erro Sintatico Linha: " + this.line.get(index) + "Token: " + this.tokens.get(index) + "\n");
+                System.exit(0);
+            }
         }
 
     }
@@ -344,20 +335,19 @@ public class GyhSint {
         if(this.tokens.get(index).equals("Var")){
             System.out.println("Token: " + this.tokens.get(index) + "\n");
             index++;
-        }else {
-            System.out.println("Erro Sintatico Linha: " + this.line.get(index) +  "Token: " + this.tokens.get(index) + "\n");
-            System.exit(0);
-        }
-
-        if(this.tokens.get(index).equals("Atrib")){
-            System.out.println("Token: " + this.tokens.get(index) + "\n");
-            index++;
+            if(this.tokens.get(index).equals("Atrib")){
+                System.out.println("Token: " + this.tokens.get(index) + "\n");
+                index++;
+                expressaoAritmetica();
+            }else{
+                System.out.println("Erro Sintatico Linha: " + this.line.get(index) +  "Token: " + this.tokens.get(index) + "\n");
+                System.exit(0);
+            }
         }else{
             System.out.println("Erro Sintatico Linha: " + this.line.get(index) +  "Token: " + this.tokens.get(index) + "\n");
             System.exit(0);
         }
 
-        expressaoAritmetica();
     }
 
     //ComandoEntrada → 'read' VARIAVEL;
@@ -365,14 +355,10 @@ public class GyhSint {
         if(this.tokens.get(index).equals("PCLer")){
             System.out.println("Token: " + this.tokens.get(index) + "\n");
             index++;
-        }else{
-            System.out.println("Erro Sintatico Linha: " + this.line.get(index) +  "Token: " + this.tokens.get(index) + "\n");
-            System.exit(0);
-        }
-
-        if(this.tokens.get(index).equals("Var")){
-            System.out.println("Token: " + this.tokens.get(index) + "\n");
-            index++;
+            if(this.tokens.get(index).equals("Var")) {
+                System.out.println("Token: " + this.tokens.get(index) + "\n");
+                index++;
+            }
         }else{
             System.out.println("Erro Sintatico Linha: " + this.line.get(index) +  "Token: " + this.tokens.get(index) + "\n");
             System.exit(0);
@@ -385,20 +371,10 @@ public class GyhSint {
         if(this.tokens.get(index).equals("PCImprimir")){
             System.out.println("Token: " + this.tokens.get(index) + "\n");
             index++;
-        }else{
-            System.out.println("Erro Sintatico Linha: " + this.line.get(index) +  "Token: " + this.tokens.get(index) + "\n");
-            System.exit(0);
-        }
-
-        if(this.tokens.get(index).equals("Var")){
+        }else if(this.tokens.get(index).equals("Var")){
             System.out.println("Token: " + this.tokens.get(index) + "\n");
             index++;
-        }else{
-            System.out.println("Erro Sintatico Linha: " + this.line.get(index) +  "Token: " + this.tokens.get(index) + "\n");
-            System.exit(0);
-        }
-
-        if(this.tokens.get(index).equals("Cadeia")){
+        }else if(this.tokens.get(index).equals("Cadeia")){
             System.out.println("Token: " + this.tokens.get(index) + "\n");
             index++;
         }else{
@@ -413,23 +389,17 @@ public class GyhSint {
         if(this.tokens.get(index).equals("PCSe")){
             System.out.println("Token: " + this.tokens.get(index) + "\n");
             index++;
+            expressaoRelacional();
+            if(this.tokens.get(index).equals("PCEntao")){
+                System.out.println("Token: " + this.tokens.get(index) + "\n");
+                index++;
+                comando();
+                comandoCondicao2();
+            }
         }else{
             System.out.println("Erro Sintatico Linha: " + this.line.get(index) +  "Token: " + this.tokens.get(index) + "\n");
             System.exit(0);
         }
-
-        expressaoRelacional();
-
-        if(this.tokens.get(index).equals("PCEntao")){
-            System.out.println("Token: " + this.tokens.get(index) + "\n");
-            index++;
-        }else{
-            System.out.println("Erro Sintatico Linha: " + this.line.get(index) +  "Token: " + this.tokens.get(index) + "\n");
-            System.exit(0);
-        }
-
-        comando();
-        comandoCondicao2();
 
     }
     //Comando ComandoCondicao'' → 'else' Comando | simboloVazio ;
@@ -450,16 +420,16 @@ public class GyhSint {
         if(this.tokens.get(index).equals("PCEnqto")){
             System.out.println("Token: " + this.tokens.get(index) + "\n");
             index++;
+            expressaoRelacional();
         }else{
             System.out.println("Erro Sintatico Linha: " + this.line.get(index) +  "Token: " + this.tokens.get(index) + "\n");
             System.exit(0);
         }
 
-        expressaoRelacional();
-
         if(this.tokens.get(index).equals("PCEntao")){
             System.out.println("Token: " + this.tokens.get(index) + "\n");
             index++;
+            comando();
         }else{
             System.out.println("Erro Sintatico Linha: " + this.line.get(index) +  "Token: " + this.tokens.get(index) + "\n");
             System.exit(0);
@@ -474,12 +444,11 @@ public class GyhSint {
         if(this.tokens.get(index).equals("PCIni")){
             System.out.println("Token: " + this.tokens.get(index) + "\n");
             index++;
+            listaComandos();
         }else{
             System.out.println("Erro Sintatico Linha: " + this.line.get(index) +  "Token: " + this.tokens.get(index) + "\n");
             System.exit(0);
         }
-
-        listaComandos();
 
         if(this.tokens.get(index).equals("PCFim")){
             System.out.println("Token: " + this.tokens.get(index) + "\n");
--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        GyhLex lex = new GyhLex(args[0]);
+        GyhLex lex = new GyhLex("C:\\Users\\Administrator\\IdeaProjects\\Compiladores\\src\\programa11.gyh");
         Token t;
         while((t = lex.nextToken()) != null) {
             System.out.println(t.toString() + " ");
